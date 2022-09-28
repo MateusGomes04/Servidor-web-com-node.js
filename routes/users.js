8b0e260b83d813455ec042c993497ff39cd84442
MateusGomes04 <mateusgomesc04@gmail.com>
2022-09-28 16:44:12 -0300

    editing user data with PUT

--- routes/users.js
+++ routes/users.js
@@ -66,4 +66,19 @@ module.exports = (app) => {
             }
         });
     });
+
+    routeId.put((req, res) => {
+
+        db.update({ _id: req.params.id }, req.body, err => {
+
+            if (err) {
+
+                app.utils.error.send(err, req, res);
+
+            } else {
+
+                res.status(200).json(Object.assign(re.params, req.body));
+            }
+        });
+    });
 };
